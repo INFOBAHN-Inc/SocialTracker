ad328be7d49e23a7594fbb5872f584f7a57e41d0
a.shigeru <a.shigeru@gmail.com>
2013-10-10 16:53:16 +0900

    Gruntfile.js 軽量化

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,62 +4,36 @@
 module.exports = function (grunt) {
     'use strict';
 
-    var _ = require('underscore'),
-        basedir        = '.',
-        config         = {},
-        jsfiles        = {},
-        closureConfig  = {},
-        watchFiles     = {};
+    var jsfiles        = [
+            '<%= basedir %>/src/defines.js',
+            '<%= basedir %>/src/analytics.js',
+            '<%= basedir %>/src/facebook.js',
+            '<%= basedir %>/src/plusone.js',
+            '<%= basedir %>/src/twitter.js',
+        ];
 
-    _([
-        'grunt-contrib-watch',
-        'grunt-closure-compiler'
-    ]).each(function (element, index, list) {
-        grunt.loadNpmTasks(element);
-    });
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-closure-compiler');
 
-    jsfiles = {
-        main: {
-            js: [
-                '<%= basedir %>/src/defines.js',
-                '<%= basedir %>/src/analytics.js',
-                '<%= basedir %>/src/facebook.js',
-                '<%= basedir %>/src/plusone.js',
-                '<%= basedir %>/src/twitter.js',
-            ],
-            jsOutputFile: '<%= basedir %>/tracking.js'
-        }
-    };
-
-    // jsFile => ClosureCompiler Setting
-    closureConfig = {};
-
-    _(jsfiles).each(function (element, key, jsfiles) {
-
-        closureConfig[key] =  {
-            js: element.js,
-            closurePath: '/usr/local/opt/closure-compiler/libexec/',
-            jsOutputFile: element.jsOutputFile,
-            options: {
-                'output_wrapper': '(function() {%output%})();'
+    grunt.initConfig({
+        'basedir': '.',
+        'closure-compiler': {
+            'main': {
+                'js': jsfiles,
+                'closurePath': '/usr/local/opt/closure-compiler/libexec/',
+                'jsOutputFile': '<%= basedir %>/tracking.js',
+                'options': {
+                    'output_wrapper': '(function(){%output%}());'
+                }
             }
-        };
+        },
+        'watch': {
+            'main': {
+                'files': jsfiles,
+                'tasks': ['closure-compiler:main']
+            }
+        }
     });
 
-    // jsFiles => Watch File List
-    watchFiles = _(jsfiles).map(function (element, key, jsfiles) {
-        return {
-            files: element.js,
-            tasks: ['closure-compiler:' + key]
-        };
-    });
-
-    config = {
-        'basedir': basedir,
-        'closure-compiler': closureConfig,
-        'watch': watchFiles
-    };
-
-    grunt.initConfig(config);
     grunt.registerTask('default', ['closure-compiler']);
 };
